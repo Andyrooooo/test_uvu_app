4b0987a2b79457839f27f96d50552d67abee0ae4
Sarah Mendez <srhmendez@gmail.com>
2025-01-14 15:52:58 -0700

    Created FileName Module

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -230,6 +230,41 @@ on:click={handleUploadModal}>Add Timeblock</button
 		>
 	</div>	
 
+	<div class="bg-white">
+		<div class="flex items-center justify-between gap-4 bg-white p-4 no-print {hiddenHeader}">
+			<div class="flex gap-8 items-center ">
+				<img src="uvu_logo2.png" alt="" class="">
+				<h1 class="text-lg lg:text-xl text-primary text-center hidden md:block font-raj transition-all duration-300">Academic Scheduling Aid</h1>
+			</div>
+			
+			<div class="">
+				<!-- <div class=""> -->
+				<div class="flex justify-end gap-4 items-center">
+					<!-- <div class="">
+						<button class="fa-solid fa-flask text-white text-2xl pl-2 hover:translate-y-[-4px] transition-all duration-200" on:click={test}></button>
+						<p class="text-center text-white">Test</p>
+					</div> -->
+
+					<!-- print button -->
+					<div class="">
+						<button on:mouseover={() => hoverPrint = true} on:mouseleave={() => hoverPrint = false} class="px-6 py-1 rounded-full {hoverPrint ? "bg-primary" : "bg-third"} shadow-sm" 
+							on:click={printScreen} >
+							<div class="{hoverPrint ? "text-white" : "text-primary"} flex items-center font-medium font-raj uppercase text-md md:text-sm transition-all duration-300" >
+								<i class="fa-solid fa-print py-0.5 md:pr-2 md:py-0"></i>
+								<p class="hidden md:block">Print Schedule</p>
+							</div>
+						</button>
+					</div>
+					
+					<!-- export button -->
+					<div class="">
+						<button on:mouseover={() => hoverExport = true} on:mouseleave={() => hoverExport = false} class="px-6 py-1 {hoverExport ? "bg-primary" : "bg-third"} rounded-full shadow-sm" on:click={exportScreenshot}>
+							<div class="{hoverExport ? "text-white" : "text-primary"} flex items-center font-medium font-raj uppercase text-md md:text-sm transition-all duration-300">
+								<i class="fa-solid fa-file-export text-md py-0.5 md:pr-2 md:py-0"></i>
+								<p class="hidden md:block">Export Schedule</p>
+							</div>
+						</button>
+						<!-- <img src="export.svg" alt="export icon" class="w-10 h-10"> -->
 	<div class="flex flex-wrap flex-col">
 		<p>Select schedule to view</p>
 		<p>reset filters</p>
@@ -381,6 +416,25 @@ on:click={handleUploadModal}>Add Timeblock</button
 				{/if}
 			
 			
+			
+		</div>
+
+		<div class="py-2 w-full border-t border-b bg-gray-100 flex gap-4 justify-end pr-4 no-print shadow-sm {hiddenHeader}"> <!-- start -->
+			<NameSchedule />
+			<!-- filter button  -->
+			<div class="">
+				<button class="px-6 py-1 bg-primary hover:bg-primaryDark rounded-full transition-all duration-300 shadow-sm" on:click={openFilterModal}>
+					<div class="text-white flex items-center font-medium font-raj uppercase text-md md:text-sm transition-all duration-300" >
+						<i class="fa-solid fa-filter py-0.5 md:pr-2 md:py-0"></i>
+						<p class="hidden md:block">Filter</p>
+					</div>
+				</button>
+
+				<div class="flex justify-center {filterSelected} transition-all duration-300 translate-y-[6px]">
+					<div class="basis-6/12  bg-primary h-[2px] rounded-full"></div>
+				</div>
+				
+				<!-- <img src="upload.svg" alt="upload icon" class="w-10 h-10">  -->
 			</div>
 
 			<!-- Import CSV button -->
